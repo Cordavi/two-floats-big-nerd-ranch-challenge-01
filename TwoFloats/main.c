c5050f77712184714fac20d70c2422289d47ccd4
Cordavi <michaelamundsen@gmail.com>
2016-01-03 12:46:14 -0500

    Completing chapter 1 challenge of Big Nerd Ranch Objective-C

---
+++ TwoFloats/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  TwoFloats
+//
+//  Created by Cordavi on 1/3/16.
+//  Copyright © 2016 Cordavi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+   
+    float pie = 3.14;
+    float fortyTwo = 42.0;
+    
+    double pieAndFortyTo = pie + fortyTwo;
+    
+    printf("Here is the result of adding pie to fortyTwo: %f\n", pieAndFortyTo);
+    
+    return 0;
+}
